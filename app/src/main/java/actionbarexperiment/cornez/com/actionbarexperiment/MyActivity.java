28d37aae60d5b609b0c11ad59f57773a0cba25ed
annguyendang14 <annguyendang14@augustana.edu>
2018-03-19 16:54:27 -0500

    Tab breakfast working (for now all tab is breakfast)

--- app/src/main/java/actionbarexperiment/cornez/com/actionbarexperiment/MyActivity.java
+++ app/src/main/java/actionbarexperiment/cornez/com/actionbarexperiment/MyActivity.java
@@ -1,7 +1,12 @@
 package actionbarexperiment.cornez.com.actionbarexperiment;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.support.v4.view.ViewPager;
 
 public class MyActivity extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class MyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager) ;
+        viewPager.setAdapter(new MyFragmentPagerAdapter(getSupportFragmentManager(),
+                MyActivity.this));
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+
+
+
+
+
+
+
+
     }
+
 }
---
+++ app/src/main/java/actionbarexperiment/cornez/com/actionbarexperiment/MyFragmentPagerAdapter.java
@@ -0,0 +1,43 @@
+package actionbarexperiment.cornez.com.actionbarexperiment;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by annguyendang14 on 3/19/2018.
+ */
+
+public class MyFragmentPagerAdapter extends FragmentPagerAdapter{
+    final int PAGE_COUNT = 4;
+    private String tabTitles[];
+    private Context context;
+
+    public MyFragmentPagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+
+        tabTitles = new String[] { context.getString(R.string.ui_tabname_breakfast), context.getString(R.string.ui_tabname_lunch), context.getString(R.string.ui_tabname_snack), context.getString(R.string.ui_tabname_dinner) };
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position==0) {
+            return new BreakfastFragment();
+        }
+        return new BreakfastFragment();
+    }
+
+}
